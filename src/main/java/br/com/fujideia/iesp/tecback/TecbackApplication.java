285273a493c1f39d15be9141d5b9e45e34456056
Santhiago Santos <santhiagosantos7@gmail.com>
2024-10-29 20:34:30 -0300

    Aula 29-10

--- src/main/java/br/com/fujideia/iesp/tecback/TecbackApplication.java
+++ src/main/java/br/com/fujideia/iesp/tecback/TecbackApplication.java
@@ -2,8 +2,12 @@ package br.com.fujideia.iesp.tecback;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@CacheConfig
+@EnableFeignClients
 public class TecbackApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/br/com/fujideia/iesp/tecback/model/dto/EnderecoDTO.java
@@ -0,0 +1,16 @@
+package br.com.fujideia.iesp.tecback.model.dto;
+
+import lombok.Data;
+
+@Data
+public class EnderecoDTO {
+    private String cep;
+    private String logradouro;
+    private String complemento;
+    private String bairro;
+    private String localidade;
+    private String uf;
+    private String estado;
+    private String regiao;
+    private String ddd;
+}
